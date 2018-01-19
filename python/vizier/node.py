46c1c9bd39583476435bc93b22e01390dc017c40
pglotfel <pglotfel@gmail.com>
2018-01-19 14:55:17 -0500

    Cleaning unsed files

--- python/vizier/node.py
+++
@@ -1,9 +0,0 @@
-class Node():
-    """
-    Wrapper class to package required functionality for nodes
-    """
-    def __init__(self, name, deps, xform, group = None):
-        self.name = name
-        self.deps = deps
-        self.group = group
-        self.xform = xform
