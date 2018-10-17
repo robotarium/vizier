70cf4538cab92fd51d11ca69d86f0383b0a885f3
pglotfel <pglotfel@gmail.com>
2018-10-17 15:34:14 -0400

    try this

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['paho.mqtt.client', 'argparse']
+MOCK_MODULES = ['paho', 'graphviz', 'argparse']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
